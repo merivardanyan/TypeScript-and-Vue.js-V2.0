3b968cd6424578b3d19a3ce29c2febc1393a6622
Meri <ms.merivardanyan@gmail.com>
2025-04-17 15:00:15 +0400

    structure change

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,24 +1,26 @@
 <template>
-  <div v-if="categoryQuery.isLoading.value">Loading category...</div>
-  <div v-if="categoryQuery.error.value">Error loading category</div>
+  <div>
+    <div v-if="categoryQuery.isLoading.value">Loading category...</div>
+    <div v-if="categoryQuery.error.value">Error loading category</div>
 
-  <div class="row pb-0">
-    <div class="col">
-      <div class="title"><router-link :to="`/`">Go Back</router-link></div>
+    <div class="row pb-0">
+      <div class="col">
+        <div class="title"><router-link :to="`/`">Go Back</router-link></div>
 
+      </div>
+      <div v-if="categoryQuery.data.value" class="col">
+        <div class="title">{{ categoryQuery.data.value.name }}</div>
+        <div v-if="categoryQuery.data.value.description" class="desc">{{ categoryQuery.data.value.description }}</div>
+      </div>
     </div>
-    <div v-if="categoryQuery.data.value" class="col">
-      <div class="title">{{ categoryQuery.data.value.name }}</div>
-      <div v-if="categoryQuery.data.value.description" class="desc">{{ categoryQuery.data.value.description }}</div>
-    </div>
-  </div>
 
-  <div v-if="productsQuery.isLoading.value">Loading products...</div>
-  <div v-if="productsQuery.error.value">Error loading products</div>
+    <div v-if="productsQuery.isLoading.value">Loading products...</div>
+    <div v-if="productsQuery.error.value">Error loading products</div>
 
-  <div v-if="productsQuery.data.value" class="products row pt-0">
-    <div v-for="product in productsQuery.data.value" :key="product.id" class="col col-1 col-sm-2 col-md-3 col-lg-5">
-      <ProductCard :product="product" />
+    <div v-if="productsQuery.data.value" class="products row pt-0">
+      <div v-for="product in productsQuery.data.value" :key="product.id" class="col col-1 col-sm-2 col-md-3 col-lg-5">
+        <ProductCard :product="product" />
+      </div>
     </div>
   </div>
 </template>
