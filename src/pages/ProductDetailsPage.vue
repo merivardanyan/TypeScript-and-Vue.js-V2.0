f9e39a2e8dfc7a947a5b5b7d131153d4e8686fd2
Meri <ms.merivardanyan@gmail.com>
2025-04-17 14:25:42 +0400

    structure and design modified

--- src/pages/ProductDetailsPage.vue
+++ src/pages/ProductDetailsPage.vue
@@ -2,16 +2,19 @@
   <div v-if="productQuery.isLoading.value">Loading product...</div>
   <div v-else-if="productQuery.error.value">Error loading product</div>
   <div v-else-if="productQuery.data.value">
-    <img
-      :src="productQuery.data.value.imageUrl"
-      :alt="productQuery.data.value.name"
-    />
-    <h1>{{ productQuery.data.value.name }}</h1>
-    <p>${{ productQuery.data.value.price }}</p>
-    <p v-html="productQuery.data.value.description"></p>
-
-    <AddToCardButton :product="productQuery.data.value"/>
-
+    <div class="row single_product">
+      <div class="col w-40 col-1">
+        <picture>
+          <img :src="productQuery.data.value.imageUrl" :alt="productQuery.data.value.name" />
+        </picture>
+      </div>
+      <div class="col w-60 info col-1">
+        <div class="title">{{ productQuery.data.value.name }}</div>
+        <div class="price">€ {{ productQuery.data.value.price }}</div>
+        <div class="desc" v-html="productQuery.data.value.description"></div>
+        <AddToCardButton :product="productQuery.data.value" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +22,32 @@
 import { useRoute } from 'vue-router';
 import AddToCardButton from '../components/AddToCardButton.vue';
 import { useProduct } from '../composables/useProduct';
+
+
 const route = useRoute();
 const productId = parseInt(route.params.id as string, 10);
 const productQuery = useProduct(productId);
 
 </script>
+
+<style scoped>
+.price {
+  font-size: 18px;
+  font-weight: 700;
+  border-bottom: 1px solid #ccc;
+}
+
+.single_product .info>div {
+  padding: 10px 0;
+}
+
+.single_product .desc>* {
+  padding: 5px 0;
+}
+
+.single_product button {
+  position: sticky;
+  bottom: 20px;
+  width: 100%;
+}
+</style>
