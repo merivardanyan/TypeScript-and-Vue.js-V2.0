464b7f4dfc5acad904e24e7e1f46971c217e7efc
Meri <ms.merivardanyan@gmail.com>
2025-04-17 14:27:42 +0400

    removed properties which are not used in 2.0 version of the project

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,22 +2,8 @@ export interface Product {
   id: number;
   name: string;
   price: number;
-  sku?: string;
   description?: string;
   imageUrl?: string;
-  thumbnailUrl?: string;
-  options?: ProductOption[];
   categoryIds?: number[];
-  defaultCategoryId?: number;
   quantity?: number;
-  defaultDisplayedPriceFormatted?: string;
-}
-
-export interface ProductOption {
-  name: string;
-  type: string;
-  choices?: {
-    text: string;
-    priceModifier?: number;
-  }[];
 }
