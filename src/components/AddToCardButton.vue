821224ce8c9b492de6fcc72fe25fa87ef489bce5
Meri <ms.merivardanyan@gmail.com>
2025-04-17 14:23:07 +0400

    button name changed to BUY  and some styles added

--- src/components/AddToCardButton.vue
+++ src/components/AddToCardButton.vue
@@ -1,21 +1,17 @@
 <template>
-    <button
-      @click="handleAdd"
-      class="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded transition"
-    >
-      Add to Cart
-    </button>
-  </template>
-  
-  <script setup lang="ts">
-  import { useCart } from '../composables/useCart';
-  import type { Product } from '../types/product';
-  
-  const props = defineProps<{ product: Product }>();
-  const { addToCart } = useCart();
-  
-  const handleAdd = () => {
-    addToCart(props.product);
-  };
-  </script>
-  
+  <button @click="() => handleAdd()">
+    Buy
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useCart } from '../composables/useCart';
+import type { Product } from '../types/product';
+
+const props = defineProps<{ product: Product }>();
+const { addToCart } = useCart();
+
+const handleAdd = () => {
+  addToCart(props.product);
+};
+</script>
