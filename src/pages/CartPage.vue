aa94794311387d71e84bb89d034ed028486c0fa2
Meri <ms.merivardanyan@gmail.com>
2025-04-17 14:09:59 +0400

    cart page and cart item styles are modified

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,107 +1,122 @@
 <template>
- <div class="col">
-  <div class="cart-page wrapper">
-    <div v-if="cartProducts.length === 0">Your cart is empty!</div>
+  <div class="col">
+    <div class="cart-page wrapper">
+      <div v-if="orderSuccess">
+        <h2 class="cart_status"><span class="pi pi-check-circle" style="color: green;"></span> Order Successful!</h2>
+        <button class="sucess_home"> <router-link to="/">
+            Go to Homepage
+          </router-link>
+        </button>
+      </div>
 
-   <div  v-else class="row flex-nowrap">
-    <div class="col">
-      <div v-for="product in cartProducts" :key="product.id" class="card-item flex justify-between">
-      <div class="flex">
-        <picture>
-        <img :src="product.imageUrl" :alt="product.name"/>
-      </picture>
-      <div class="flex flex-col justify-between">
-        <h3>{{ product.name }}</h3>
-        <p>{{ product.price }} $</p>
-        <p>Quantity: {{ product.quantity }}</p>
-      </div>
-      </div>
-      <div>
-        <button @click="removeFromCart(product.id)" class="bg-red-600 text-white p-2 rounded">Remove</button>
+      <div class="cart_status" v-else-if="cartProducts.length === 0">Your cart is empty!</div>
+
+      <div v-else class="row">
+        <div class="col col-1 cart_items w-40">
+            <CartItem v-for="product in cartProducts" :key="product.id" :product="product"
+              @changeQuantity="changeQuantity" @removeFromCart="removeFromCart" />
+        </div>
+
+        <div class="col col-1 total w-60">
+          <div class="wrapper ">
+            <div class="summary">Order Summary</div>
+            <div> <span class="gray_text">Total Items: </span><span class="bold_600">{{ totalItems }}</span></div>
+            <div> <span class="gray_text"> Total Price: </span><span class="bold_600">€ {{ totalPrice.toFixed(2) }}</span></div>
+          <div class="flex"><button @click="clearCart">Clear Cart</button>
+            <button v-if="totalItems > 0" @click="placeOrder">Place Order</button></div>
+          </div>
+        </div>
+
       </div>
     </div>
-    </div>
-
-    <div class="col">
-      <p>Total Items: {{ totalItems }}</p>
-      <p>Total Price: ${{ totalPrice.toFixed(2) }}</p>
-      <button @click="clearCart">Clear Cart</button>
-      <button v-if="totalItems > 0" @click="placeOrder">Place Order</button>
-    </div>
-
-   </div>
-    
   </div>
-  <div v-if="showOverlay" class="overlay">
-  <div class="message-box">
-    <h2>🎉 Congratulations!</h2>
-    <p>Your order has been placed successfully.</p>
-  </div>
-</div>
- </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 import { useCart } from '../composables/useCart';
-const showOverlay = ref(false);
-const router = useRouter();
-
+import CartItem from '../components/CartItem.vue';
 const {
   cartProducts,
   totalItems,
   totalPrice,
   removeFromCart,
-  clearCart
+  clearCart,
+  changeQuantity
 } = useCart();
-function placeOrder() {
-  showOverlay.value = true;
-  clearCart();
+const orderSuccess = ref(false);
 
-  setTimeout(() => {
-    showOverlay.value = false;
-    router.push('/');
-  }, 3000);
+function placeOrder() {
+  orderSuccess.value = true;
+  clearCart();
 }
 
 </script>
 
-<style scoped>
-.cart-page {
-  padding: 20px;
-}
-picture{
+<style>
+.cart_item img{
+  width: 100px;
   height: 100px;
-  min-width: 100px;
-  max-width: 100px;
   object-fit: contain;
-}
-img{
-  object-fit: contain;
-  max-height: 100%;
-}
-.card-item{
-  padding: 10px;
-}
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
+  object-position: left top;
 }
 
-.message-box {
+.cart_item{
+  margin-bottom: 10px;
+  padding: 10px;
+  box-shadow: 1px 3px 3px #cccccc5e;
+}
+
+.quantity span{
+  display: inline-block;
+    min-width: 20px;
+    padding: 7px 10px;
+    font-size: 10px;
+    margin-inline: 5px;
+    border-radius: 5px;
+    color: #545454;
+    cursor: pointer;
+    background: #f1f1f1;
+}
+
+.remove{
+  font-size: 22px;
+  color: rgba(255, 0, 0, 0.656);
+  padding-left: 10px;
+  cursor: pointer;
+}
+.cart_items .title{
+  font-size: 14px;
+}
+.total .wrapper{
+  
   background: white;
-  padding: 40px;
-  border-radius: 10px;
+    border: 1px solid #cccccc5e;
+    padding: 20px;
+    position: sticky;
+    top: 90px;
+}
+.gray_text{
+  color: gray;
+}
+.bold_600{
+  font-weight: 600;
+}
+
+.total .wrapper>div{
+  padding-bottom: 10px;
+}
+.total .summary{
+  font-weight: 600;
+  font-size: 18px;
+  padding-bottom: 20px;
+}
+.cart_status{
+  padding: 20px;
   text-align: center;
 }
-
+.sucess_home{
+  width: auto;
+  margin: 0 auto;
+}
 </style>
