a95129440dba71e60b60781134b3c8bc5f665622
Meri <ms.merivardanyan@gmail.com>
2025-04-21 15:18:35 +0400

    publicPath removed

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    publicPath: '/TypeScript-and-Vue.js-V2.0/', // Replace with your GitHub repo name
-  }
+// module.exports = {
+//     publicPath: '/TypeScript-and-Vue.js-V2.0/', // Replace with your GitHub repo name
+//   }
